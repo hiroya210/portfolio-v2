e0f262f7caf0c994fa708db9b903db44e653c292
Hiroya Takemura <hirotakemura210@gmail.com>
2022-04-14 10:28:53 +0900

    some minor adjustment

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,17 +1,27 @@
 import React,{useEffect, useRef} from 'react'
 
-const Canvas = (props) => {
+const Canvas = ({draw}) => {
     const canvasRef = useRef(null);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
+        let frameCount = 0;
+        let animationFrameId;
         //draw
-        context.fillStyle = '#000000'
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-    }, [])
+        const render = () => {
+            frameCount++
+            draw(context, frameCount)
+            animationFrameId = window.requestAnimationFrame(render)
+          }
+          render()
+          
+          return () => {
+            window.cancelAnimationFrame(animationFrameId)
+          }
+    }, [draw])
 
-    return <canvas ref={canvasRef} {...props}/>
+    return <canvas ref={canvasRef}/>
 }
 
 export default Canvas
